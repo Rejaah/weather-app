844ceb222c5b1026b0fb93a52163ad2189ad6b2c
KarRz <reza11781@gmail.com>
2025-07-24 18:45:06 +0700

    feat: add daily forecast

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
 import HourlyForecast from "./components/Forecast/HourlyForecast/HourlyForecast";
+import DailyForecast from "./components/Forecast/DailyForecast/DailyForecast";
 import Search from "./components/Search/Search";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import "./App.css";
@@ -8,7 +9,34 @@ import "./App.css";
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const [background, setBackground] = useState("bg-default");
 
+  const getBackground = (icon) => {
+    const code = icon.slice(0,2);
+    const time = icon.slice(2);
+
+    const isNight = time === "n";
+
+    switch (code) {
+      case '01': 
+        return isNight ? 'bg-night-clear' : 'bg-day-clear';
+      case '02':
+      case '03':
+      case '04': 
+        return isNight ? 'bg-night-cloudy' : 'bg-day-cloudy';
+      case '09': 
+      case '10':
+        return isNight ? 'bg-night-rain' : 'bg-day-rain';
+      case '11':
+        return isNight ? 'bg-night-thunderstorm' : 'bg-day-thunderstorm';
+      case '13':
+        return isNight ? 'bg-night-snow' : 'bg-day-snow';
+      case '50':
+        return isNight ? 'bg-night-mist' : 'bg-day-mist';
+      default:
+        return 'bg-default';
+    }
+  }
   const handleSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
@@ -22,6 +50,9 @@ function App() {
 
         setCurrentWeather({city: searchData.label, ...weatherResponse});
         setForecast({city: searchData.label, ...forecastResponse});
+
+        const backgroundClass = getBackground(weatherResponse.weather[0].icon);
+        setBackground(backgroundClass);
       })
       .catch((err) => console.error(err));
   };
@@ -31,12 +62,13 @@ function App() {
 
   return (
     <>
-      <div className="app-background bg-day-clear"></div>
+      <div className={`app-background ${background}`}></div>
       <div className="main-container">
         <h1 className="title">Weather App</h1>
         <Search onSearchChange={handleSearchChange} />
         {currentWeather && <CurrentWeather data={currentWeather} />}
         {forecast && <HourlyForecast data={forecast} />}
+        {forecast && <DailyForecast data={forecast} />}
       </div>
     </>
   );
---
+++ src/components/Forecast/DailyForecast/DailyForecast.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./DailyForecast.css"
+
+const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const DailyForecast = ({ data }) => {
+    const today = new Date().getDay();
+    const forecastDays = DAYS.slice(today, DAYS.length).concat(DAYS.slice(0, today));
+
+    const dailyForecasts = data.list.filter(item => item.dt_txt.includes("00:00:00"));
+
+    return (
+        <div className="daily-forecast-card">
+            <h2 className="daily-forecast-title">Daily Forecast</h2>
+            {dailyForecasts.map((item, idx) => (
+                <details className="daily-forecast-container" key={idx}>
+                    <summary className="daily-item-summary">
+                        <div className="daily-item">
+                            <img src={`/assets/icons/${item.weather[0].icon}.png`} alt="" />
+                            <label className="day">{forecastDays[idx]}</label>
+                            <label className="description">{item.weather[0].description}</label>
+                            <label className="temp-range">High: {Math.round(item.main.temp_max)}°C | Low: {Math.round(item.main.temp_min)}°C</label>
+                        </div>
+                    </summary>
+                    <div className="daily-details-grid">
+                        <div className="grid-item"><label>Pressure:</label><label>{item.main.pressure} hPa</label></div>
+                        <div className="grid-item"><label>Humidity:</label><label>{item.main.humidity}%</label></div>
+                        <div className="grid-item"><label>Clouds:</label><label>{item.clouds.all}%</label></div>
+                        <div className="grid-item"><label>Wind speed:</label><label>{item.wind.speed} m/s</label></div>
+                        <div className="grid-item"><label>Sea level:</label><label>{item.main.sea_level}m</label></div>
+                        <div className="grid-item"><label>Feels like:</label><label>{Math.round(item.main.feels_like)}°C</label></div>
+                    </div>
+                </details>
+            ))}
+        </div>
+    )
+}
+
+export default DailyForecast;
